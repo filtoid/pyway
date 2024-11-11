c937c2d5a7fcb6568f6894b88726d6e1fc07b38d
filtoid <philjeffes@hotmail.com>
2024-11-11 20:09:20 +0100

    add option to skip postgres tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 # Taken from https://stackoverflow.com/a/42145604 
 def pytest_addoption(parser):
     parser.addoption("--skip-mysqld-tests", action="store", default="False")
-    pass 
+    parser.addoption("--skip-postgres-tests", action="store", default="False")
 
 
 # This is called for every test. Only get/set command line arguments
@@ -14,3 +14,9 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "mysqld_test" in item.keywords:
                 item.add_marker(skip_mysqld)
+
+    if config.getoption("--skip-postgres-tests") and config.getoption("--skip-postgres-tests").lower() == "true":
+        skip_postgres = pytest.mark.skip(reason="skipped due to postgres requirement")
+        for item in items:
+            if "postgresql_test" in item.keywords:
+                item.add_marker(skip_postgres)
