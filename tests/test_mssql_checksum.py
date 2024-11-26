403971a698f7d507116705139ceb8a88f019a73c
filtoid <philjeffes@hotmail.com>
2024-11-26 08:35:48 +0100

    moving MSSQL object into project tests set

--- tests/test_mssql_checksum.py
+++ tests/test_mssql_checksum.py
@@ -8,11 +8,45 @@ from pyway.settings import ConfigFile
 # from mysqld_integration_test import Mysqld
 import pyodbc
 
-from mssql_integration_test import MsSql
+class MsSql():
+    def __init__(self, host, db_name, driver, username=None, password=None, port=1433):
+        if host is None:
+            raise ValueError("Host cannot be None")
+        
+        if db_name is None:
+            raise ValueError("Database Name cannot be None")
+
+        if driver is None:
+            raise ValueError("Driver cannot be None")
+
+        self.host = host
+        self.port = port
+        self.db_name = db_name
+        self.username = username
+        self.password = password
+        self.driver = driver
+
+        self.__engine__ = None
+        self.__conn__ = None
+
+    def connect(self):
+        if self.__conn__ is None:
+            conn_str  = f"DRIVER={{{self.driver}}};SERVER={self.host},{self.port};" \
+                        f"DATABASE={self.db_name};"
+            if self.username is not None and self.password is not None:
+                conn_str = f"{conn_str}UID={self.username};PWD={self.password};" \
+                            "TrustServerCertificate=yes;"
+            else:
+                conn_str = f"{conn_str}Trusted_Connection=yes;" \
+                            "TrustServerCertificate=yes;INTEGRATED " \
+                            "SECURITY=yes;ENCRYPT=no;"
+            self.__conn__ = pyodbc.connect(conn_str)
+        return self.__conn__
+
 
 @pytest.fixture
 def mssql_connect(autouse: bool = True) -> MsSql:
-    mssql = MsSql(host='mssql', db_name='PywayTests', user='sa', pw='p@ssw0rd!', driver='ODBC Driver 18 for SQL Server')
+    mssql = MsSql(host='mssql', db_name='PywayTests',  driver='ODBC Driver 18 for SQL Server', username='sa', password='p@ssw0rd!')
     return mssql.connect()
 
 
